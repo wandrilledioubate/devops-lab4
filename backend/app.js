fee353c8119cb665e2c4394cc6ab013e0ed059d4
Wandrille Dioubate <wandrille.dioubate@efrei.net>
2024-11-26 16:48:55 +0100

    add mongo log to backend

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const mongoURI = process.env.MONGO_URL || "YOUR_MONGO_DB_CONNECTION_STRING";
+const mongoURI = process.env.MONGO_URL || "mongodb+srv://wandrille:wandrillepw@devops-lab.sohwk.mongodb.net/";
 mongoose.connect(mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
